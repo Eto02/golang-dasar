78513ccdf9d059b758ef1929c9fef64c075caca3
Tahta Failah Mubarak <mmikoto89@gmail.com>
2024-07-07 15:15:36 +0700

    test go-my-module

---
+++ app-my-module/go.mod
@@ -0,0 +1,7 @@
+module app-my-module
+
+go 1.22.5
+
+require (
+	github.com/Eto02/go-my-module v1.5.0 // indirect
+)
---
+++ app-my-module/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gomymodule "github.com/Eto02/go-my-module"
+)
+
+func main() {
+	fmt.Println(gomymodule.SayHello())
+}
